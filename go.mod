5b5f7774a3dd0ed887679647507cafe6e72ac3c2
Mason <109301662+CodedMasonry@users.noreply.github.com>
2025-02-22 22:04:31 -0500

    add env parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module collegemap
 
 go 1.24.0
+
+require (
+	github.com/charmbracelet/log v0.4.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
+	"time"
 
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
+)
+
+var (
+	IMAPUser    string // IMAP_USER
+	IMAPPass    string // IMAP_PASSWORD
+	IMAPAddress string // IMAP_ADDRESS, default 0.0.0.0:1443
+)
+
+func main() {
+	// Changes the logger to charmbracelet/log
+	// Call slog so logging can be agnostic
+	logger := log.NewWithOptions(os.Stderr, log.Options{
+		TimeFormat:      time.DateTime,
+		ReportTimestamp: true,
+	})
+	slogger := slog.New(logger)
+	slog.SetDefault(slogger)
+
+	// Load .env (for convenience)
+	err := godotenv.Load()
+	if err != nil {
+		slog.Warn("No .env Detected; continuing...")
+	}
+
+	// Parse Env variables
+	IMAPUser = os.Getenv("IMAP_USER")
+	IMAPPass = os.Getenv("IMAP_PASSWORD")
+	IMAPAddress = os.Getenv("IMAP_ADDRESS")
+	if IMAPUser == "" {
+		log.Fatal("`IMAP_USER` not set, required variable")
+	}
+	if IMAPPass == "" {
+		log.Fatal("`IMAP_PASSWORD` not set, required variable")
+	}
+	if IMAPAddress == "" {
+		log.Warn("No `IMAP_ADDRESS` set, using default 0.0.0.0:1143")
+		IMAPAddress = "0.0.0.0:1143"
+	}
 }
